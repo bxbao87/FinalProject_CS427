3a569fcea5f188feec15c083fe54cee1fc999fb5
LHN2225 <50903203+LHN2225@users.noreply.github.com>
2021-09-09 22:28:35 +0700

    add card and 3 sprite of Wolf, Hare, Deer

---
+++ FinalARProject/Assets/Script/Card.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card
+{
+    public string CardName;
+
+    public Sprite CardCharacter;
+    public Sprite CardBackground;
+    public bool IsUnlocked;
+
+    /* Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
