868844b4212600a3004d59a350f964a03a72c161
bxbao87 <bxbao18@apcs.vn>
2021-09-19 12:19:57 +0700

    [update] filter

--- FinalARProject/Assets/Script/FaceMaskChange.cs
+++ FinalARProject/Assets/Script/FaceMaskChange.cs
@@ -9,18 +9,36 @@ public class FaceMaskChange : MonoBehaviour
     [SerializeField]
     ARFaceManager faceManager;
 
+    public AudioSource audioSource;
+    public string sound;
+    AudioClip clip;
+    float timer = 0.0f;
+
     void Awake()
     {
         Screen.orientation = ScreenOrientation.Portrait;
 
-        string Name = "AR " + PlayerPrefs.GetString(Constant.prefAnimal, Constant.foodChainCommon);
-        var loadedPrefabResource = ControlDisplayScene.LoadPrefabFromFile(Name);
+        string Name = PlayerPrefs.GetString(Constant.prefAnimal, Constant.foodChainCommon);
+        var loadedPrefabResource = ControlDisplayScene.LoadPrefabFromFile("AR " + Name);
         faceManager.facePrefab = Instantiate(loadedPrefabResource) as GameObject;
+
+        clip = Resources.Load<AudioClip>("Sound/" + Name);
+        if (clip == null) Debug.Log("null clip");
+        if (clip != null && sound == "True") audioSource.PlayOneShot(clip, 1.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (clip != null)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > clip.length - 2 && sound == "True")
+            {
+                audioSource.PlayOneShot(clip, 1.0f);
+                timer = 0;
+            }
+        }
     }
+
 }
